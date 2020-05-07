6e88bd54e98daf7fcbfb96cc64d50318552af2bf
Thabo Mayika <mayikathabo@gmail.com>
2020-05-07 21:50:46 +0200

    2nd commit

--- app.py
+++ app.py
@@ -1,59 +1,67 @@
 import dash
 import dash_html_components as html
 import dash_core_components as dcc
-import plotly.graph_objs as go
 import plotly.express as px
-import seaborn as sns
-import pandas as pd 
-import numpy as np
-from dash.dependencies import Input, Output
+
+
+import pandas as pd
+
 global df
 df = pd.read_csv('insurance.csv')
-
-app = dash.Dash()
-
+df_sort = df.sort_values(by='charges',)
 
 
-app.layout = html.Div([
-                    
-                    dcc.Graph(id='scatterplot', 
-                        figure = px.scatter(df, x="bmi", y="charges", 
-                            color="charges", title='cheapest fee and BMI')),
-    
-                    dcc.Graph(id='piechart', 
-                        figure = px.pie(df, values='charges', names='region', 
-                            color_discrete_sequence=px.colors.sequential.RdBu, 
-                            title='Cheapest insurance fee by region')),
-
-                    dcc.Graph(id='bargraph', 
-                        figure = px.bar(df, x=df['age'], y=df['charges'],
-                            hover_data=['age', 'charges'],
-                            height=650, title='cheapest insurance fee by age')),
-
-                    dcc.Graph(id='stacked bargraph',
-                        figure = px.bar(df, x="sex", y="charges", 
-                            height=650, 
-                            title='gender that pays the least fee')),    
-
-                    dcc.Graph(id='grouped bargraph', 
-                        figure = px.bar(df, x="sex", y="charges", color='smoker', 
-                            barmode ='group', height=650, 
-                            title='gender that pays the least fee')),   
-
-                    dcc.Interval(id='interval-component', 
-                        interval=2000,
-                        n_intervals=0)
+def generate_table(dataframe, max_rows=10):
+    return html.Table([
+        html.Thead(
+            html.Tr([html.Th(col) for col in dataframe.columns]),
+            title='lowest charges(highest fee is 63770,43)'
+        ),
+        html.Tbody([
+            html.Tr([
+                html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+            ]) for i in range(min(len(dataframe), max_rows))
+        ])
+    ])
 
 
+app = dash.Dash(__name__)
+app.layout = html.Div([generate_table(df_sort),
+
+                       dcc.Graph(id='piechart',
+                                 figure=px.pie(df,
+                                               values='charges',
+                                               names='region',
+                                               color='region',
+                                               title='Percentage Fees By Region')),
+
+                       dcc.Graph(id='bargraph',
+                                 figure=px.bar(df, x=df['age'],
+                                               y=df['charges'],
+                                               hover_data=['age', 'charges'],
+                                               height=650,
+                                               title='insurance fee by age')),
+
+                       dcc.Graph(id='stacked bargraph',
+                                 figure=px.bar(df, x="sex", y="charges",
+                                               height=650,
+                                               title='Gender paying least fee')),
+
+                       dcc.Graph(id='grouped bargraph',
+                                 figure=px.bar(df, x="sex", y="charges",
+                                               color='smoker',
+                                               barmode='group',
+                                               height=650,
+                                               title='Rates by gender(Smoker and non-Smoker)')),
+
+                       dcc.Interval(id='interval-component', interval=2000,
+                                    n_intervals=0)
 
 
-                    ])
 
 
+                       ])
 
-            
-                            
 
 if __name__ == '__main__':
-    app.run_server(8058)
-
+    app.run_server(debug=True)
